e5d4dce4c2e96dc67ebfb2dd9df6889be947d37a
Corentin <cortech12@gmail.com>
2021-02-15 11:40:23 +0100

    creation commande.vue

--- src/components/Commande.vue
+++ src/components/Commande.vue
@@ -1,13 +1,34 @@
 <template>
   <v-container>
-    <v-row class="text-center"> </v-row>
+    <v-row class="text-center"> 
+
+   <ul id="example-1">
+  <li v-for="commande in commande" :key="commande.id">
+    {{ commande }}
+  </li>
+</ul>
+    
+         <button type="button" v-on:click="abbort()">Annuler la commande</button>
+        <button type="button" v-on:click="payment()">payer</button>
+    </v-row>
   </v-container>
 </template>
+
 
 <script>
 export default {
   name: "Commande",
 
-  data: () => ({})
+  data: () => ({
+    commandes: "../assets/data/commandes.json"
+  }),
+  computed: {
+    abbort: function() {
+      //suppression de la commande
+      },
+         payer: function() {
+      //suppression de la commande
+      },
+     }
 };
 </script>
