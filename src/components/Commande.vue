831d366ddda52adafb3611387deb0bb1aae5ab07
Corentin <cortech12@gmail.com>
2021-02-15 17:08:55 +0100

    liset commande avec paniezr actuel

--- src/components/Commande.vue
+++ src/components/Commande.vue
@@ -2,23 +2,28 @@
 <template>
   <v-container>
     <h1>Liste des Commandes</h1>
-  <li v-for="item in this.indexCommandesDisplay" :key="item.message">     
+  <!-- <li v-if="this.panierActual != []">  -->
+     <!-- {{this.panierActual}} -->
+<!-- <li  v-for="item in this.panierActual[0]" :key="item.message">   -->
+    <!-- {{this.panierActual}} -->
+      <v-data-table
+      :headers="headers"
+      :items="panierActual"
+      item-key="id"
+      class="elevation-1"
+    >
+    </v-data-table>
+ <!-- </li> -->
+<!-- </li> -->
+ <li v-for="item in this.indexCommandesDisplay" :key="item.message">     
+<!-- {{commandes[item].articles}} -->
       <v-data-table
       :headers="headers"
       :items="commandes[item].articles"
       item-key="id"
       class="elevation-1"
-      :search="search"
-      :custom-filter="filterOnlyCapsText"
     >
-      <template v-slot:top>
-      </template>
-      <template v-slot:body.append>
-        <tr>
-          <td></td>
-          <td colspan="4"></td>
-        </tr>
-      </template>
+     
     </v-data-table>
  </li>
   </v-container>
@@ -37,8 +42,12 @@ data: () => ({
   commandes: jsonFile,
   indexCommandesDisplay : null,
   user: null,
+  panierActual: null
   }),
   mounted() {
+    this.panierActual = []
+    this.panierActual = JSON.parse(localStorage.getItem('panier'))
+    console.log(this.panierActual[0])
     this.indexCommandesDisplay = []
     this.local
      for (var index in jsonFile) {
