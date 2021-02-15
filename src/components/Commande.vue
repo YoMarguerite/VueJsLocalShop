84e01166e66ca15b80cbc9d9a2c687f1687b3d4d
Corentin <cortech12@gmail.com>
2021-02-15 13:08:15 +0100

    commande

--- src/components/Commande.vue
+++ src/components/Commande.vue
@@ -1,50 +1,65 @@
 <template>
   <v-container>
-    <v-row > 
+    <h1>Liste des articles</h1>
+    {{displayCommande}}
+  <li v-for="(item, index) in indexCommandesDisplay">
+       {{ index }} - {{ item }}
+    <p> Article </p>
+  <p> Nom</p>
+     <p> Description</p>
+      <p> Prix</p>
 
-    <ul>
-      <li v-for="commande in commande" :key="commande.id">
-      {{ commande }}
-      </li>
-    </ul>
-    
+ </li>
     <button type="button" v-on:click="abbort()">Annuler la commande</button>
     <button type="button" v-on:click="payment()">payer</button>
-    </v-row>
+
   </v-container>
 </template>
 
 
+
+
+
 <script>
+import jsonFile from '../assets/data/commandes.json'
 export default {
   name: "Commande",
 
 data: () => ({
-  commandes: "../assets/data/commandes.json",
+  commandes: jsonFile,
+  displayCommande : null,
+  indexCommandesDisplay : [],
   
   }),
   mounted() {
-    //Récupération des commande
-    // this.commandes = localStorage.
-    //   ? JSON.parse(localStorage.panier) : null
-    console.log(this.commandes);
+    // this.commandes = JSON.parse(this.commandes)
+     for (var index in jsonFile) {
+       if(this.commandes[index].user.name === "Corentin"){
+         console.log(index)        
+         this.indexCommandesDisplay.push(index)
+          console.log(" ---- indexCommandesDisplay", this.indexCommandesDisplay)
+         }
+      }
+      console.log('//')
+      console.log(this.indexCommandesDisplay);
+      console.log('//')
   },
   computed: {
+      headers () {
+      return [
+        { text: "id", value: "id" },
+        { text: "Nom", value: "name" },
+        { text: "Description", value: "description" },
+        { text: "Prix", value: "price" } 
+      ]
+    },
     abbort: function() {
       //suppression de la commande
-      for (let commande of this.commandes) {
-        // if(){
-
-        // }
-      console.log(commande);
-      
-      }
       return true
       },
          payer: function() {
       //suppression de la commande
       return true
-
       },
      }
 };
