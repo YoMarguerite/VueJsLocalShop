a7d74cc421e9a2c61788f1ad2c850712780bdbc1
Yoann Marguerite <yoann.marguerite@orange.fr>
2021-03-22 09:47:45 +0100

    faq

---
+++ src/components/FAQ.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container class="mt-1">
+    <v-row>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>FAQ</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Details",
+
+  data: () => ({}),
+  mounted() {}
+};
+</script>
